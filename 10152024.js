c4d619c1a52ba1ce8ed36170f847d485b49e019f
Sudarsan-Ganesan <68058752+Sudarsan-Ganesan@users.noreply.github.com>
2024-10-15 13:55:26 +0530

    Update 10152024.js
    For loop statement with two variables

--- 10152024.js
+++ 10152024.js
@@ -1,3 +1,4 @@
+/* Fibonacci */
 function printFibonacci(n) {
     let a = 0, b = 1, c;
     console.log(a);
@@ -11,3 +12,17 @@ function printFibonacci(n) {
   }
   
   printFibonacci(10);
+
+
+/* For loop with two variables */  
+function num(numbers) {
+  let num = numbers[0];
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      let z = i+j;
+      console.log(z);
+    }
+  }
+}
+
+num([5]);
