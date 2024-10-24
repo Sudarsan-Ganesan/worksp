e51cfa7a30f11f65fbd3f322dd0fdb6845593e47
Sudarsan-Ganesan <68058752+Sudarsan-Ganesan@users.noreply.github.com>
2024-10-24 23:57:59 +0530

    Update 10242024.js
    Find the biggest from given number

--- 10242024.js
+++ 10242024.js
@@ -1,3 +1,4 @@
+/* Palindrome in checker */
 function isPalindrome(str) {
     let j = str.length - 1
     for (let i = 0; i < str.length / 2; i++) {
@@ -16,3 +17,26 @@ let str3 = "Rama";
 console.log(isPalindrome(str1));
 console.log(isPalindrome(str2));
 console.log(isPalindrome(str3));
+
+
+/* Find the biggest in given number */
+        function findBiggest(numbers) {
+          if (numbers.length === 0) {
+            return undefined; // Handle empty array case
+          }
+        
+          let biggest = numbers[0]; // Assume the first number is the biggest
+        
+          for (let i = 1; i < numbers.length; i++) {
+            if (numbers[i] > biggest) {
+              biggest = numbers[i];
+            }
+          }
+        
+          return biggest;
+        }
+        
+        /* const numbers = [10, 5, 8, 20, 3, 6, 17, 15]; */
+        const biggestNumber = findBiggest(numbers);
+        
+        console.log("The biggest number is:", biggestNumber); // Output: 20
