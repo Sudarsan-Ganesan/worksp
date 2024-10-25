013ec4478a561eafde35dd20f0a83560146a911d
Sudarsan-Ganesan <68058752+Sudarsan-Ganesan@users.noreply.github.com>
2024-10-25 23:24:11 +0530

    Create 10252024.js
    Sort

---
+++ 10252024.js
@@ -0,0 +1,7 @@
+    const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+    fruits.sort();
+    console.log("After sort:", fruits);    // Output after sort()
+
+    fruits.reverse();
+    console.log("After reverse:", fruits); // Output after reverse()
